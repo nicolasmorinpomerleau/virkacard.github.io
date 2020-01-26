67c6bd571c55c153f5dcd2aee2e84d5b8f2442e9
nicolas <nicolasmorinpomerleau@gmail.com>
2020-01-25 20:57:42 -0500

    add the insert function
    in index.html and main.js

--- calendar/main.js
+++ calendar/main.js
@@ -1,7 +1,15 @@
+// ****************************************
+// Set up the endDate To display the busy
+// ****************************************
 var someDate = new Date();
 var numberOfDaysToAdd = 10;
 someDate.setDate(someDate.getDate() + numberOfDaysToAdd); 
+// ****************************************
 
+
+// *****************************************
+// Function publicCalendar()
+// *****************************************
 function publicCalendar() {
         gapi.client.calendar.events.list({
           // 'calendarId': 'primary',
@@ -29,4 +37,48 @@ function publicCalendar() {
             appendPre('No upcoming events found yoyo.');
           }
         });
-      }
+      } // Function publicCalendar
+// ****************************************
+
+// ****************************************
+// Function insert()
+// ****************************************
+function insert() {
+      var event = {
+        'summary': 'Google I/O 2015',
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+          'dateTime': '2020-02-28T09:00:00-05:00'
+        //   'timeZone': 'America/Los_Angeles'
+        },
+        'end': {
+          'dateTime': '2020-01-28T17:00:00-05:00'
+        //   'timeZone': 'America/Los_Angeles'
+        },
+        'recurrence': [
+          'RRULE:FREQ=DAILY;COUNT=2'
+        ],
+        'attendees': [
+          {'email': 'nicolasmorinpomerleau@gmail.com'}
+        ],
+        'reminders': {
+          'useDefault': false,
+          'overrides': [
+            {'method': 'email', 'minutes': 24 * 60},
+            {'method': 'popup', 'minutes': 10}
+          ]
+        }
+      };
+      
+      var request = gapi.client.calendar.events.insert({
+        'calendarId': 'u82n0vas2g2ahb8u5c5c3d8vu4@group.calendar.google.com',
+        'resource': event
+      });
+      
+      request.execute(function(event) {
+        appendPre('Event created: ' + event.htmlLink);
+      });
+
+    } // Function insert()
+    // ****************************************
