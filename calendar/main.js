f22febcf740d1d9aea60a33a5b182d6bf7e5794f
nicolas <nicolasmorinpomerleau@gmail.com>
2020-01-25 19:31:47 -0500

    add main.js  file

---
+++ calendar/main.js
@@ -0,0 +1,27 @@
+function publicCalendar() {
+        gapi.client.calendar.events.list({
+          // 'calendarId': 'primary',
+          'calendarId':'u82n0vas2g2ahb8u5c5c3d8vu4@group.calendar.google.com',
+          'timeMin': (new Date()).toISOString(),
+          'showDeleted': false,
+          'singleEvents': true,
+          'maxResults': 10,
+          'orderBy': 'startTime'
+        }).then(function(response) {
+          var events = response.result.items;
+          appendPre('Upcoming events yep:');
+
+          if (events.length > 0) {
+            for (i = 0; i < events.length; i++) {
+              var event = events[i];
+              var when = event.start.dateTime;
+              if (!when) {
+                when = event.start.date;
+              }
+              appendPre(event.summary + ' (' + when + ')')
+            }
+          } else {
+            appendPre('No upcoming events found yoyo.');
+          }
+        });
+      }
